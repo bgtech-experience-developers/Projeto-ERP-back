794d001b2ab25ac698cf33b18350446997ecf44f
lucas <lucassidpaula@gmail.com>
2024-10-09 14:08:57 -0300

    refactor(cliente): refatorando a parte de registro de usuário e implementando a segurança de criptografia de senha

--- src/app/controllers/clientesController.js
+++ src/app/controllers/clientesController.js
@@ -1,36 +1,47 @@
 import { ClientesRepository } from "../repository/ClientesRepository.js";
 
-const {criar, mostrar, deletar} = new ClientesRepository
+const { criar, mostrar, deletar } = new ClientesRepository();
 
 export class ClienteController {
-
-    async mostrar(req, res) {
-        try {
-            const clientes = await mostrar()
-            res.json(clientes)
-        } catch(error) {
-            res.send(error)
-        }
+  async mostrar(req, res) {
+    try {
+      const clientes = await mostrar();
+      res.json(clientes);
+    } catch (error) {
+      res.send(error);
     }
+  }
 
-    async criar(req, res) {
-        try {
-            const {nome, cpf, email, senha, telefone, situacao} = req.body
-            const cliente = await criar(cpf, nome, email, senha, telefone, situacao)
-            res.status(200).json({message: 'Cliente cadastrado com sucesso!'})
-
-        } catch (error) {
-            res.status(500).json({message: 'Erro interno do servidor'})
-        }
+  async criar(req, res) {
+    try {
+      const { nome, cpf, email, senha, telefone, situacao } = req.body;
+      const { cadastrarCliente, message } = await criar(
+        cpf,
+        nome,
+        email,
+        senha,
+        telefone,
+        situacao
+      );
+      res
+        .status(200)
+        .json("sua senha foi criptografada" + cadastrarCliente.senha);
+    } catch (error) {
+      res.status(500).json({ message: "Erro interno do servidor" });
     }
+  }
 
-    async deletar(req, res) {
-        try {
-            const {cpf} = req.body
-            const clienteDeletado = await deletar(cpf)
-            res.json(clienteDeletado.message)
-        } catch (error) {
-            res.status(500).json({message: 'Não foi possivel deletar o cliente' + error.message})
-        }
+  async deletar(req, res) {
+    try {
+      const { cpf } = req.body;
+      const clienteDeletado = await deletar(cpf);
+      res.json(clienteDeletado.message);
+    } catch (error) {
+      res
+        .status(500)
+        .json({
+          message: "Não foi possivel deletar o cliente" + error.message,
+        });
     }
+  }
 }
--- src/app/repository/ClientesRepository.js
+++ src/app/repository/ClientesRepository.js
@@ -1,46 +1,53 @@
 import { instanciaPrisma } from "../database/conexao.js";
+import byccript from "bcrypt";
 
 export class ClientesRepository {
-    async criar(nome, cpf, email, senha, telefone, situacao) {
-        try {
-            const cliente = await instanciaPrisma.clientes.findUnique({where: {cpf}})
-            if (!cliente) {
-                const cadastrarCliente = await instanciaPrisma.clientes.create({data: {nome, cpf, email, senha, telefone, situacao}})
-                const message = {message: 'Cadastro realizado com sucesso!'}
-                return {
-                    cadastrarCliente,
-                    message
-                } 
-            } else {
-                throw new Error('Dados inválidos!')
-            }
-        } catch (error) {
-            throw new Error(error)
-        }
+  async criar(nome, cpf, email, senha, telefone, situacao) {
+    try {
+      const cliente = await instanciaPrisma.clientes.findUnique({
+        where: { cpf },
+      });
+      if (!cliente) {
+        const senhaHas = byccript.hashSync(senha, 10);
+        const cadastrarCliente = await instanciaPrisma.clientes.create({
+          data: { nome, cpf, email, senha: senhaHas, telefone, situacao },
+        });
+        const message = { message: "Cadastro realizado com sucesso!" };
+        return {
+          cadastrarCliente,
+          message,
+        };
+      } else {
+        throw new Error("Dados inválidos!");
+      }
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    async mostrar() {
-        try {
-            return await instanciaPrisma.clientes.findMany()
-        } catch (error) {
-            throw new Error(error)
-        }
+  async mostrar() {
+    try {
+      return await instanciaPrisma.clientes.findMany();
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    async deletar(cpf) {
-        try {
-            const cliente = await instanciaPrisma.clientes.findUnique({where: {cpf}})
-            if (!cliente) {
-                throw new Error('Cliente não encontrado ou não existe')
-            }
+  async deletar(cpf) {
+    try {
+      const cliente = await instanciaPrisma.clientes.findUnique({
+        where: { cpf },
+      });
+      if (!cliente) {
+        throw new Error("Cliente não encontrado ou não existe");
+      }
 
-            await instanciaPrisma.clientes.delete({where: {cpf}})
+      await instanciaPrisma.clientes.delete({ where: { cpf } });
 
-            const message = {message: 'Cliente deletado com sucesso'}
-            return message
-
-        } catch (error) {
-            throw new Error(error)
-        }
+      const message = { message: "Cliente deletado com sucesso" };
+      return message;
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 }
