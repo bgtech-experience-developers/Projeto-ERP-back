a6ace28b68e863dfaac266221cbbed283cab70d6
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2024-10-09 16:19:08 -0300

    feat: adicionado as rotas para funcionarios

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import rotaCliente from './app/routes/clientesRotas.js'
+import rotaFuncionarios from './app/routes/funcionarioRotas.js'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/clientes', rotaCliente)
+app.use('/funcionarios', rotaFuncionarios)
 
 const PORTA = process.env.PORTA
 
