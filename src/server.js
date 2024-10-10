e95c78e8b6395908a3cff277b79e511793c64019
lucas <lucassidpaula@gmail.com>
2024-10-10 11:49:48 -0300

    fix(telefone):ajeitando um problema de tipagem na coluna de telefone modificando de inteiro para string

--- src/server.js
+++ src/server.js
@@ -3,15 +3,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import rotaCliente from "./app/routes/clientesRotas.js";
-import swaggerjson from "../swagger.json" assert { type: "json" };
-import swaggerui from "swagger-ui-express";
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerjson));
 
 app.use("/clientes", rotaCliente);
 
