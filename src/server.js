d92065f6a118ed50950936211912ad9c5cae2e57
gabrielcaetano1 <gabrielcaetano2319@gmail.com>
2024-10-10 15:51:45 -0300

    feat: adicionado as rotas para os endpoints de fornecedor

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import rotaCliente from "./app/routes/clientesRotas.js";
+import rotaFornecedor from "./app/routes/fornecedoresRotas.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/clientes", rotaCliente);
+app.use('/fornecedores', rotaFornecedor);
 
 const PORTA = process.env.PORTA;
 
