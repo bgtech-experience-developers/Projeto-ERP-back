4d1cb510deae437416028a23fe3c1d02b705825e
lucas <lucassidpaula@gmail.com>
2024-10-23 23:29:41 -0300

    feat: iniciando o projeto em ts

--- js.js
+++
@@ -1,7 +0,0 @@
-let data = "2005-02-19";
-
-const date = new Date(`${data}T00:00:00Z`);
-const data1 = "10-10-2005";
-const regex = /[./-/]/gi;
-const dataorgi = data1.replace(regex, "-").split("-").reverse().join("-");
-console.log(dataorgi);
--- src/app/middleware/errorclient.js
+++
@@ -1,9 +0,0 @@
-import { HandlerError } from "../error/errorhandler.js";
-
-export const erroClient = (err, req, res, next) => {
-  if (err instanceof HandlerError) {
-    res.status(err.status).json({ message: err.message });
-    return;
-  }
-  res.status(500).json({ message: `erro interno de servidor` });
-};
