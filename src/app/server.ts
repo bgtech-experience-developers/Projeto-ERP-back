c38ff01b51fcda5a7ff69026381361a018649c93
unknown <levysousa6211@digitalcollege.internal>
2024-11-26 12:02:43 -0300

    Atualizando o serve para ser 0.0.0.0

--- src/app/server.ts
+++ src/app/server.ts
@@ -3,16 +3,18 @@ import dotnev from "dotenv";
 import { clientRouter } from "./router/Client.js";
 import cors from "cors";
 import { routerAdm } from "./router/adm.js";
+import { number } from "joi";
 
 dotnev.config();
 const app = express();
-const port = process.env.PORT;
+const port = Number(process.env.PORT);
+const host = "0.0.0.0"
 app.use("/files", express.static("uploads")); // diretório acessível para requisições vindo do cliente, tendo acesso à arquivos hospedado internamente dentro dessa pasta!
 app.use(cors());
 app.use(express.json()); // parte para a deserializaç~~ao das informações
 app.use("/cliente", clientRouter);
 app.use("/adm", routerAdm);
-
-app.listen(port, () => {
+app.listen()
+app.listen(port, host,  () => {
   console.log("meu servidor está rodando na porta " + port);
 });
