680c8d0dec8a3056d67e88f9645f27645d15d5dc
unknown <levysousa6211@digitalcollege.internal>
2024-11-26 16:37:14 -0300

    Ajustes

--- src/app/server.ts
+++ src/app/server.ts
@@ -9,12 +9,12 @@ dotnev.config();
 const app = express();
 const port = Number(process.env.PORT);
 const host = "0.0.0.0"
+
 app.use("/files", express.static("uploads")); // diretório acessível para requisições vindo do cliente, tendo acesso à arquivos hospedado internamente dentro dessa pasta!
 app.use(cors());
 app.use(express.json()); // parte para a deserializaç~~ao das informações
 app.use("/cliente", clientRouter);
 app.use("/adm", routerAdm);
-app.listen()
 app.listen(port, host,  () => {
   console.log("meu servidor está rodando na porta " + port);
 });
