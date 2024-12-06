e128be7ed8adb1620d1313f04f8f72b19f829bf0
Levy <85988919231levy@gmail.com>
2024-12-06 00:30:31 -0300

    ajustado a pasta uploads

--- src/app/utils/sharp.ts
+++ src/app/utils/sharp.ts
@@ -1,44 +1,4 @@
-// import fs from "fs";
-// import sharp from "sharp";
-// import { AllError } from "../error/AllError.js";
-// export class SharpLimpeza {
-//   static async ValidatationFiles(
-//     files: Express.Multer.File[],
-//     order: boolean[]
-//   ): Promise<AllImagens> {
-//     try {
-//       files.forEach(async (files) => {
-//         if (files.mimetype === "jpeg" || files.mimetype === "png") {
-//           const path = files.path;
-//           const outPutFilePath = `uploads/sanitized_${files.originalname}`;
-//           await sharp(path).toFormat("png").toFile(outPutFilePath);
-//           fs.rename(outPutFilePath, path, (err) => {
-//             if (err) {
-//               throw new AllError("não foi possivel sobrecrever a imagem", 500);
-//             } else {
-//               console.log(
-//                 "foi possivel atulizar a imagem agora sanitizada e sem metadados e scripts maliciosos"
-//               );
-//             }
-//           });
-//         } else {
-//           throw new AllError(
-//             "formato inesperado de arquivo, envie somente jpeg ou png"
-//           );
-//         }
-//       });
-//       return order.map((boolean, index) => {
-//         if (boolean) {
-//           return files[index].filename;
-//         } else {
-//           return null;
-//         }
-//       });
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-// }
+
 import { NextFunction } from "express";
 import fs from "fs";
 import sharp from "sharp";
