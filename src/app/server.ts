79d412b3540de4276d1ff3df45b7442768807e86
lucas <lucassidpaula@gmail.com>
2025-01-09 15:12:51 -0300

    refactor: removendo a chave ssl que foi criada pro seervidor

--- src/app/server.ts
+++ src/app/server.ts
@@ -3,16 +3,11 @@ import dotnev from "dotenv";
 import { clientRouter } from "./router/Client.js";
 import cors from "cors";
 import { routerAdm } from "./router/adm.js";
-
 import { number } from "joi";
 import cookieParse from "cookie-parser";
 import Supplier_pf from "./router/SupplierPf.js";
 import supplierPf from "./router/SupplierPf.js";
 import { supplierPjRouter } from "./router/supplierPj.js";
-import https from "https";
-import fs, { opendir } from "fs";
-https.createServer();
-
 dotnev.config();
 const app = express();
 const port = Number(process.env.PORT);
@@ -25,24 +20,14 @@ app.use(
     credentials: true,
   })
 );
-const options = {
-  key: fs.readFileSync(
-    "/etc/letsencrypt/live/erp-homologation.bgtech.com.br/privkey.pem"
-  ),
-  cert: fs.readFileSync(
-    "/etc/letsencrypt/live/erp-homologation.bgtech.com.br/fullchain.pem"
-  ),
-};
+
 app.use(cookieParse());
 app.use(express.json()); // parte para a deserializaç~~ao das informações
 app.use("/clientes", clientRouter);
 app.use("/adm", routerAdm);
 app.use("/fornecedor/fisico", supplierPf);
 app.use("/fornecedor/juridico", supplierPjRouter);
-https.createServer(options, app).listen(433, host, () => {
-  "meu server está rodando em http";
-});
 
-// app.listen(port, host, () => {
-//   console.log("meu servidor está rodando na porta " + port);
-// });
+app.listen(3500, host, () => {
+  console.log("meu servidor está rodando na porta " + port);
+});
