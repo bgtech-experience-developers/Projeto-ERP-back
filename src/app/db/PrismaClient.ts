1c9d8800267b39aa931bf11a20ee5aecf560f114
Levy <85988919231levy@gmail.com>
2025-01-13 17:23:29 -0300

    mudanças

--- src/app/db/PrismaClient.ts
+++ src/app/db/PrismaClient.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 export class InstanciaPrisma {
   static prisma: PrismaClient = new PrismaClient({ log: ["query"] });
 
-  static  GetConnection(): PrismaClient {
+  static GetConnection(): PrismaClient {
     return this.prisma
       ? this.prisma
       : (this.prisma = new PrismaClient({ log: ["query"] }));
