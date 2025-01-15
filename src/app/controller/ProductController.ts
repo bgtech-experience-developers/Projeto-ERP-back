21faf4bb9d56cf3c8ea7ac7ea67832d749b73cb9
Alex Bessa <alexbessa@mail.com>
2025-01-15 14:24:35 -0300

    Ajustando router/Product

--- src/app/controller/ProductController.ts
+++ src/app/controller/ProductController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express';
 import ProductService from '../service/ProductService.js';
 
 export default class ProductController {
-  static async createProduct(req: Request, res: Response) {
+  static async createProduct(req: Request, res: Response): Promise<Response> {
     try {
       const data = req.body;
       const product = await ProductService.createProduct(data);
       return res.status(201).json(product);
-    } catch (error) {
-      return res.status(500).json({ message: error });
+    } catch (error: any) {
+      return res.status(500).json({ message: error.message || 'Error creating product' });
     }
   }
 }
