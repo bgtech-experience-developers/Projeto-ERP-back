566a6d7cfd030777648d0694dfcedb53ff70fafb
Levy <85988919231levy@gmail.com>
2025-01-28 08:49:12 -0300

    update:joi refactor

--- src/app/utils/joi.ts
+++ src/app/utils/joi.ts
@@ -27,6 +27,7 @@ export class JoiValidation {
       fantasy_name: joi.string().trim().allow(""),
       corporate_reason: joi.string().trim(),
     });
+
     const SchemaCreateSector = joi.object<$Interface, false, $Interface>({
       cell_phone: joi.string().trim().allow(""),
       name: joi.string().trim().allow(""),
@@ -43,6 +44,7 @@ export class JoiValidation {
         })
         .allow(""),
     });
+    
     const SchemaAddress = joi.object<$Interface, false, $Interface>({
       cep: joi
         .string()
@@ -70,7 +72,7 @@ export class JoiValidation {
       SchemaAddress.validate(endereco_empresa),
       SchemaAddress.validate(endereco_entrega),
     ]);
-  }
+  } 
 
     static async schemaLogin(body: login) {
     try {
