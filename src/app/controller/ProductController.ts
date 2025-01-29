cadb148ba5e8d1e3a8b55318cd8e4e3272fdd4a3
Levy <85988919231levy@gmail.com>
2025-01-29 16:55:57 -0300

    feat(controller): implementação da controller de countAll

--- src/app/controller/ProductController.ts
+++ src/app/controller/ProductController.ts
@@ -1,23 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
 import ProductService from '../service/ProductService.js';
 
-export default class ProductController {
-  static async createProduct(req: Request, res: Response): Promise<Response> {
+export  class ProductController {
+  // static async createProduct(req: Request, res: Response) {
+  //   try {
+  //     const data = req.body;
+  //     const product = await ProductService.createProduct(data);
+  //     return res.status(201).json(product);
+  //   } catch (error: any) {
+  //     // return 
+  //     res.status(500).json({ message: error.message || 'Error creating product' });
+
+      
+  //   }
+  // }
+
+  static async getAll(request: Request<any, any, any, {take: string, skip: string}>, response: Response, next: NextFunction) {
     try {
-      const data = req.body;
-      const product = await ProductService.createProduct(data);
-      return res.status(201).json(product);
-    } catch (error: any) {
-      return res.status(500).json({ message: error.message || 'Error creating product' });
+      const productAll = await ProductService.getAll(request.query.take, request.query.skip);
+      response.status(200).json(productAll);
+      return
+    } catch(error) {
+        next(error)
     }
   }
 
-  static async getAll(request: Request<any, any, any, {take: string, skip: string}>, responde: Response, next: NextFunction) {
+  static async countAll(request: Request, response: Response, next: NextFunction) {
     try {
-      const productAll = await ProductService.getAll(request.query.take, request.query.skip);
-      return responde.status(200).json(productAll);
+      const countProduct = await ProductService.countAll();
+      response.status(200).json(countProduct);
+      return
     } catch(error) {
-        next(error)
+        next(error);
     }
   }
 }
