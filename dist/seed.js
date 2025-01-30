92bb359b2580a18815f25b0e5e1f5c4b4087a13b
lucas <lucassidpaula@gmail.com>
2025-01-30 19:25:15 -0300

    feat: finalizando a conexão das tabelas

--- dist/seed.js
+++ dist/seed.js
@@ -1,9 +1,6 @@
-"use strict";
-// import { log } from "console";
-// import { InstanciaPrisma } from "./db/PrismaClient.js";
-// import { fa, faker, fakerPL } from "@faker-js/faker"; // Biblioteca para gerar dados aleatórios
-// import { read } from "fs";
-// const connectionDb = InstanciaPrisma.GetConnection();
+import { InstanciaPrisma } from "./db/PrismaClient.js";
+import { faker } from "@faker-js/faker"; // Biblioteca para gerar dados aleatórios
+const connectionDb = InstanciaPrisma.GetConnection();
 // // async function main() {
 // //     const data = new Date()
 // //     data.getDate()
@@ -257,176 +254,6 @@
 // //   console.log("100 registros criados com sucesso! de clientes");
 // // }
 // main()
-<<<<<<< HEAD
-=======
-// .then(async () => {
-//     await connectionDb.$disconnect()
-// })
-// .catch(async (e) =>{
-//     console.error(e)
-//     await connectionDb.$disconnect()
-//     process.exit(1)
-// })
-async function main() {
-    const records = [];
-    for (let i = 1; i <= 100; i++) {
-        const birthDate = faker.date.past({ years: 50 }); // Gera uma data de nascimento aleatória
-        records.push(connectionDb.supplier_pf.create({
-            data: {
-                supplier_name: faker.company.name(),
-                supplier_code: faker.string.numeric(4), // Substitui faker.random.numeric
-                cpf: faker.string.numeric(11), // Substitui faker.random.numeric
-                email: faker.internet.email(),
-                rg: faker.string.numeric(9), // Substitui faker.random.numeric
-                birth_date: birthDate,
-                phone: "85912345678",
-                supplier_imagem: {
-                    create: {
-                        supplier_pf_image: {
-                            create: {
-                                path: faker.image.url(),
-                            },
-                        },
-                    },
-                },
-                address_supplier_pf: {
-                    create: {
-                        address: {
-                            create: {
-                                cep: faker.location.zipCode("########"), // Atualizado para faker.location
-                                street: faker.location.street(),
-                                number: faker.string.numeric(3),
-                                complement: faker.location.secondaryAddress(),
-                                city: faker.location.city(),
-                                neighborhood: faker.location.county(),
-                            },
-                        },
-                    },
-                },
-            },
-        }));
-    }
-    await connectionDb.$transaction(records);
-    console.log("100 registros criados com sucesso!");
-}
-async function client() {
-    const record = [];
-    for (let i = 1; i <= 100; i++) {
-        record.push(connectionDb.client.create({
-            data: {
-                cnpj: faker.string.numeric(14),
-                situation: faker.datatype.boolean(),
-                branch_activity: faker.person.jobArea(),
-                state_registration: faker.string.numeric(9),
-                fantasy_name: faker.company.name(),
-                corporate_reason: faker.company.name(),
-                type_contribuition: faker.company.name(),
-                accounting_contact: {
-                    create: {
-                        sector: {
-                            create: {
-                                name: faker.person.fullName(),
-                                phone: faker.string.numeric(10),
-                                cell_phone: faker.string.numeric(8),
-                                cpf: faker.string.numeric(11),
-                                email: faker.internet.email(),
-                                rg: faker.string.numeric(7),
-                                accounting_contact_image: {
-                                    create: { image: { create: { path: faker.image.url() } } },
-                                },
-                            },
-                        },
-                    },
-                },
-                owner_partner: {
-                    create: {
-                        sector: {
-                            create: {
-                                name: faker.person.fullName(),
-                                phone: faker.string.numeric(10),
-                                cell_phone: faker.string.numeric(8),
-                                cpf: faker.string.numeric(11),
-                                email: faker.internet.email(),
-                                rg: faker.string.numeric(7),
-                                owner_partner_image: {
-                                    create: { image: { create: { path: faker.image.url() } } },
-                                },
-                            },
-                        },
-                    },
-                },
-                commercial_contact: {
-                    create: {
-                        sector: {
-                            create: {
-                                name: faker.person.fullName(),
-                                phone: faker.string.numeric(10),
-                                cell_phone: faker.string.numeric(8),
-                                cpf: faker.string.numeric(11),
-                                email: faker.internet.email(),
-                                rg: faker.string.numeric(7),
-                                commercial_image: {
-                                    create: { image: { create: { path: faker.image.url() } } },
-                                },
-                            },
-                        },
-                    },
-                },
-                delivery_address: {
-                    create: {
-                        adress: {
-                            create: {
-                                cep: faker.location.zipCode("########"),
-                                street: faker.location.street(),
-                                number: faker.string.numeric(3),
-                                complement: faker.location.secondaryAddress(),
-                                city: faker.location.city(),
-                                neighborhood: faker.location.county(),
-                            },
-                        },
-                    },
-                },
-                financinal_contact: {
-                    create: {
-                        sector: {
-                            create: {
-                                name: faker.person.fullName(),
-                                phone: faker.string.numeric(10),
-                                cell_phone: faker.string.numeric(8),
-                                cpf: faker.string.numeric(11),
-                                email: faker.internet.email(),
-                                rg: faker.string.numeric(7),
-                                financial_image: {
-                                    create: { image: { create: { path: faker.image.url() } } },
-                                },
-                            },
-                        },
-                    },
-                },
-                image_company: {
-                    create: { image: { create: { path: faker.image.url() } } },
-                },
-                company_address: {
-                    create: {
-                        adress: {
-                            create: {
-                                cep: faker.location.zipCode("########"),
-                                street: faker.location.street(),
-                                number: faker.string.numeric(3),
-                                complement: faker.location.secondaryAddress(),
-                                city: faker.location.city(),
-                                neighborhood: faker.location.county(),
-                            },
-                        },
-                    },
-                },
-            },
-        }));
-    }
-    await connectionDb.$transaction(record);
-    console.log("100 registros criados com sucesso! de clientes");
-}
-// main();
 async function pj() {
     try {
         const record = [];
@@ -480,7 +307,6 @@ async function pj() {
 //     process.exit(1);
 //   });
 // client()
->>>>>>> a4b710e9182275a574610f189b78e400e2d72434
 //   .then(async () => {
 //     await connectionDb.$disconnect();
 //   })
---
+++ prisma/migrations/20250130222325_init_tables_again/migration.sql
@@ -0,0 +1,447 @@
+-- CreateTable
+CREATE TABLE `client` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `corporate_reason` VARCHAR(1000) NOT NULL,
+    `fantasy_name` VARCHAR(1000) NULL,
+    `branch_activity` VARCHAR(1000) NULL,
+    `cnpj` VARCHAR(14) NOT NULL,
+    `state_registration` VARCHAR(100) NULL,
+    `type_contribuition` VARCHAR(191) NULL,
+    `situation` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sector` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(1000) NULL,
+    `email` VARCHAR(100) NULL,
+    `phone` VARCHAR(100) NULL,
+    `cell_phone` VARCHAR(100) NULL,
+    `rg` VARCHAR(191) NULL,
+    `cpf` VARCHAR(11) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `imagem` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `path` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `cep` VARCHAR(8) NULL,
+    `street` VARCHAR(100) NULL,
+    `number` VARCHAR(100) NULL,
+    `complement` VARCHAR(100) NULL,
+    `city` VARCHAR(100) NULL,
+    `neighborhood` VARCHAR(100) NULL,
+    `state` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `emails` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adm_id` INTEGER NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pj_address` (
+    `id_supplier` INTEGER NOT NULL,
+    `id_address` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id_address`, `id_supplier`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pf` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `supplier_name` VARCHAR(255) NOT NULL,
+    `supplier_code` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(150) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `rg` VARCHAR(15) NOT NULL,
+    `cpf` VARCHAR(11) NOT NULL,
+    `birth_date` DATETIME(3) NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `supplier_pf_cpf_key`(`cpf`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pf_image` (
+    `id_supplier_pf` INTEGER NOT NULL,
+    `id_image` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id_image`, `id_supplier_pf`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pf_address` (
+    `id_supplier_pf` INTEGER NOT NULL,
+    `id_address` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id_address`, `id_supplier_pf`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pj` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_imagem` INTEGER NOT NULL,
+    `corporate_reason` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+    `fantasy_name` VARCHAR(191) NULL,
+    `cnpj` VARCHAR(14) NOT NULL,
+    `answerable` VARCHAR(191) NULL,
+    `state_registration` VARCHAR(191) NULL,
+    `type_contribuition` VARCHAR(191) NULL,
+    `municipal_registration` VARCHAR(191) NULL,
+    `suframa_registration` VARCHAR(191) NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `supplier_pj_cnpj_key`(`cnpj`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tempory_token` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adm_id` INTEGER NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `company_address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adressId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `commercial_image` (
+    `imageId` INTEGER NOT NULL,
+    `commercial_contactId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`imageId`, `commercial_contactId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `image_company` (
+    `companyId` INTEGER NOT NULL,
+    `imageId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`companyId`, `imageId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `delivery_address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `adressId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `financial_image` (
+    `imageId` INTEGER NOT NULL,
+    `financial_contactId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`imageId`, `financial_contactId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `financial_contact` (
+    `sectorId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`sectorId`, `clientId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `owner_partner_image` (
+    `imageId` INTEGER NOT NULL,
+    `owner_partnerId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`imageId`, `owner_partnerId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `owner_partner` (
+    `sectorId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`sectorId`, `clientId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `commercial_contact` (
+    `sectorId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`sectorId`, `clientId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounting_contact_image` (
+    `imageId` INTEGER NOT NULL,
+    `accounting_contactId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`imageId`, `accounting_contactId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `accounting_contact` (
+    `sectorId` INTEGER NOT NULL,
+    `clientId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`sectorId`, `clientId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `adm` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `cnpj` CHAR(14) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `adm_cnpj_key`(`cnpj`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `role` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `role_name` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `roleadm` (
+    `adm_id` INTEGER NOT NULL,
+    `role_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`adm_id`, `role_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_image` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `supplier_name` VARCHAR(191) NOT NULL,
+    `serie_number` INTEGER NOT NULL,
+    `barcode` INTEGER NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `cost_value` VARCHAR(191) NOT NULL,
+    `weight` VARCHAR(191) NOT NULL,
+    `width` VARCHAR(191) NOT NULL,
+    `height` VARCHAR(191) NOT NULL,
+    `length` VARCHAR(191) NOT NULL,
+    `description` TEXT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `product_barcode_key`(`barcode`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pf_product` (
+    `id_product` INTEGER NOT NULL,
+    `id_supplier_pf` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id_product`, `id_supplier_pf`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `supplier_pj_product` (
+    `id_product` INTEGER NOT NULL,
+    `id_supplier_pj` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `update_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id_product`, `id_supplier_pj`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `emails` ADD CONSTRAINT `emails_adm_id_fkey` FOREIGN KEY (`adm_id`) REFERENCES `adm`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pj_address` ADD CONSTRAINT `supplier_pj_address_id_supplier_fkey` FOREIGN KEY (`id_supplier`) REFERENCES `supplier_pj`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pj_address` ADD CONSTRAINT `supplier_pj_address_id_address_fkey` FOREIGN KEY (`id_address`) REFERENCES `address`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_image` ADD CONSTRAINT `supplier_pf_image_id_supplier_pf_fkey` FOREIGN KEY (`id_supplier_pf`) REFERENCES `supplier_pf`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_image` ADD CONSTRAINT `supplier_pf_image_id_image_fkey` FOREIGN KEY (`id_image`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_address` ADD CONSTRAINT `supplier_pf_address_id_supplier_pf_fkey` FOREIGN KEY (`id_supplier_pf`) REFERENCES `supplier_pf`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_address` ADD CONSTRAINT `supplier_pf_address_id_address_fkey` FOREIGN KEY (`id_address`) REFERENCES `address`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pj` ADD CONSTRAINT `supplier_pj_id_imagem_fkey` FOREIGN KEY (`id_imagem`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `tempory_token` ADD CONSTRAINT `tempory_token_adm_id_fkey` FOREIGN KEY (`adm_id`) REFERENCES `adm`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `company_address` ADD CONSTRAINT `company_address_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `company_address` ADD CONSTRAINT `company_address_adressId_fkey` FOREIGN KEY (`adressId`) REFERENCES `address`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commercial_image` ADD CONSTRAINT `commercial_image_commercial_contactId_fkey` FOREIGN KEY (`commercial_contactId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commercial_image` ADD CONSTRAINT `commercial_image_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `image_company` ADD CONSTRAINT `image_company_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `image_company` ADD CONSTRAINT `image_company_companyId_fkey` FOREIGN KEY (`companyId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `delivery_address` ADD CONSTRAINT `delivery_address_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `delivery_address` ADD CONSTRAINT `delivery_address_adressId_fkey` FOREIGN KEY (`adressId`) REFERENCES `address`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial_image` ADD CONSTRAINT `financial_image_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial_image` ADD CONSTRAINT `financial_image_financial_contactId_fkey` FOREIGN KEY (`financial_contactId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial_contact` ADD CONSTRAINT `financial_contact_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial_contact` ADD CONSTRAINT `financial_contact_sectorId_fkey` FOREIGN KEY (`sectorId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `owner_partner_image` ADD CONSTRAINT `owner_partner_image_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `owner_partner_image` ADD CONSTRAINT `owner_partner_image_owner_partnerId_fkey` FOREIGN KEY (`owner_partnerId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `owner_partner` ADD CONSTRAINT `owner_partner_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `owner_partner` ADD CONSTRAINT `owner_partner_sectorId_fkey` FOREIGN KEY (`sectorId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commercial_contact` ADD CONSTRAINT `commercial_contact_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commercial_contact` ADD CONSTRAINT `commercial_contact_sectorId_fkey` FOREIGN KEY (`sectorId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `accounting_contact_image` ADD CONSTRAINT `accounting_contact_image_accounting_contactId_fkey` FOREIGN KEY (`accounting_contactId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `accounting_contact_image` ADD CONSTRAINT `accounting_contact_image_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `accounting_contact` ADD CONSTRAINT `accounting_contact_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `client`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `accounting_contact` ADD CONSTRAINT `accounting_contact_sectorId_fkey` FOREIGN KEY (`sectorId`) REFERENCES `sector`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `roleadm` ADD CONSTRAINT `roleadm_adm_id_fkey` FOREIGN KEY (`adm_id`) REFERENCES `adm`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `roleadm` ADD CONSTRAINT `roleadm_role_id_fkey` FOREIGN KEY (`role_id`) REFERENCES `role`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `product` ADD CONSTRAINT `product_id_image_fkey` FOREIGN KEY (`id_image`) REFERENCES `imagem`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_product` ADD CONSTRAINT `supplier_pf_product_id_product_fkey` FOREIGN KEY (`id_product`) REFERENCES `product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pf_product` ADD CONSTRAINT `supplier_pf_product_id_supplier_pf_fkey` FOREIGN KEY (`id_supplier_pf`) REFERENCES `supplier_pf`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pj_product` ADD CONSTRAINT `supplier_pj_product_id_product_fkey` FOREIGN KEY (`id_product`) REFERENCES `product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `supplier_pj_product` ADD CONSTRAINT `supplier_pj_product_id_supplier_pj_fkey` FOREIGN KEY (`id_supplier_pj`) REFERENCES `supplier_pj`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
